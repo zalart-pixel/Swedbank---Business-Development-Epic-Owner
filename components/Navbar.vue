a533ea2a3936bff803ea777d7894e9dcee31c1da
zalart-pixel <zalitisarturs@gmail.com>
2025-11-20 12:54:51 +0200

    Update Navbar branding from 'Nuxt Starter' to 'Inspire Analysis'

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -16,7 +16,7 @@ const menuitems = [
     <header class="flex flex-row flex-wrap items-center justify-between my-4 gap-x-6 gap-y-4">
       <div class="flex items-center justify-between">
         <a href="/" class="text-2xl font-bold text-zinc-800">
-          <span class="text-green-500">&#10070;</span> Nuxt Starter
+          <span class="text-green-500">&#10070;</span> Inspire Analysis
         </a>
       </div>
       <nav class="flex w-auto">
